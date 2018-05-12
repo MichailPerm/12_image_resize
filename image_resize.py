0c1d84f5d9aac1637d3cb4cf69c822aab8d9e1bb
Michail_Z <apxahrejl@gmail.com>
2018-05-12 08:37:42 +0500

    Image resizing program first release

--- image_resize.py
+++ image_resize.py
@@ -6,6 +6,9 @@ import sys
 # TODO сделать ограничение на допустимые размеры (масштаб)
 # TODO сформировать имя нового рисунка и путь его сохранения
 
+WIDTH_INDEX = 0
+HEIGHT_INDEX = 1
+
 
 def add_arguments():
     parser = argparse.ArgumentParser()
@@ -38,6 +41,8 @@ def process_args(size_params):
     check_argument_presence_and_value(args.scale, 'scale', size_params, type='float')
     if any(key in ['width', 'height'] for key in size_params.keys()) and 'scale' in size_params.keys():
         raise AttributeError
+    if args.output and not os.path.isdir(args.output):
+        raise RuntimeError
     return args
 
 
@@ -50,7 +55,7 @@ def compute_result_size(source_size, size_params):
     if 'scale' in size_params:
         return (int(source_size['width'] * size_params['scale']), int(source_size['height'] * size_params['scale']),)
     if 'width' in size_params and 'height' in size_params:
-        print("Scale of source image will not be saved")
+        print('Scale of source image will not be saved')
         return (size_params['width'], size_params['height'],)
     if 'width' in size_params:
         height = int((size_params['width'] / source_size['width']) * source_size['height'])
@@ -60,23 +65,38 @@ def compute_result_size(source_size, size_params):
         return (width, size_params['height'],)
 
 
-def resize_image(source_image, source_size, size_params):
-    result_size = compute_result_size(source_size, size_params)
-    resized_image = source_image.resize(result_size)
+def resize_image(source_image, source_size, size_params, output_size):
+    output_size_tuple = compute_result_size(source_size, size_params)
+    output_size['width'] = output_size_tuple[WIDTH_INDEX]
+    output_size['height'] = output_size_tuple[HEIGHT_INDEX]
+    resized_image = source_image.resize(output_size_tuple)
     return resized_image
 
 
 def get_size_from_thumbnails(source_image):
-    width_index = 0
-    height_index = 1
     size = {}
-    size['width'] = source_image.size[width_index]
-    size['height'] = source_image.size[height_index]
+    size['width'] = source_image.size[WIDTH_INDEX]
+    size['height'] = source_image.size[HEIGHT_INDEX]
     return size
+
+
+def save_image(args, resized_image, output_size):
+    source_image_dirs, source_image_name = os.path.split(args.filepath)
+    source_image_name_parts = source_image_name.split('.')
+    output_image_name = '{}__{}x{}.{}'.format(source_image_name_parts[0], output_size['width'], output_size['height'], source_image_name_parts[1])
+    if not getattr(args, 'output'):
+        output_path = source_image_dirs + '/' + output_image_name
+        message = 'Image saved at {} as {}'.format(source_image_dirs, output_image_name)
+    else:
+        output_path = args.output + output_image_name
+        message = 'Image saved at {} as {}'.format(args.output, output_image_name)
+    resized_image.save(output_path)
+    return message
 
 
 if __name__ == '__main__':
     size_params = {}
+    output_size = {}
     try:
         args = process_args(size_params)
     except IOError:
@@ -85,12 +105,16 @@ if __name__ == '__main__':
         sys.exit('Incompatible size arguments introduced. Need arguments: width or height (or both), or scale only!')
     except TypeError:
         sys.exit('Size arguments must be only numeric!')
+    except RuntimeError:
+        sys.exit('Path to store output file is not correct!')
     try:
         source_image = open_image(args.filepath)
         source_size = get_size_from_thumbnails(source_image)
-        resize_image(source_image, source_size, size_params)
-        # resized_image = resize_image(source_image, source_size, size_params)
+        resized_image = resize_image(source_image, source_size, size_params, output_size)
         source_image.close()
+        message = save_image(args, resized_image, output_size)
+        print(message)
+    except PermissionError:
+        sys.exit('Something wrong with a file. Check if output path correct!')
     except IOError:
         sys.exit('Unable to open file {}!'.format(args.filepath))
-
