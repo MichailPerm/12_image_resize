f22679d2c77643964c787669587a2aa7db8d121a
Michail_Z <apxahrejl@gmail.com>
2018-05-14 10:01:54 +0500

    First release of image resizing program

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Image Resizer
 
-[TODO. There will be project description]
+This program creates resized image from source by given scale or width/height.
+
+# Quick Start
+
+Before start, you need to install Pillow library (if you have not it).
+To get help of this program, type in command line:
+```bash
+python image_resize.py -h
+```
+
+Example of program works with scale given:
+```bash
+(lenv) user@Ihome:~/sproj/12_image_resize$ python image_resize.py -s 3 -f ~/Downloads/test_image.png 
+Image saved at /home/user/Downloads as test_image__1500x1500.png
+```
+
+Example of program works with scale and output path given:
+```bash
+(lenv) user@Ihome:~/sproj/12_image_resize$ python image_resize.py -s 3 -f ~/Downloads/test_image.png -o /home/user/
+Image saved at /home/user/Downloads as test_image__1500x1500.png
+```
+
+Launch in Windows is the same.
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -3,15 +3,15 @@ from PIL import Image
 import os
 import sys
 
-# TODO сделать ограничение на допустимые размеры (масштаб)
-# TODO сформировать имя нового рисунка и путь его сохранения
 
 WIDTH_INDEX = 0
 HEIGHT_INDEX = 1
 
 
 def add_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        epilog='REMEMBER! Too big size or scale may'
+               'cause your computer freeze!')
     parser.add_argument(
         '-f', '--filepath',
         help='path to original image file')
@@ -30,10 +30,10 @@ def add_arguments():
     return parser.parse_args()
 
 
-def check_argument_presence_and_value(argument,
-                                      argument_name,
-                                      dictionary,
-                                      type='int'):
+def check_size_argument_presence_and_value(argument,
+                                           argument_name,
+                                           dictionary,
+                                           type='int'):
     if argument and argument.isnumeric():
         if type == 'int':
             dictionary[argument_name] = int(argument)
@@ -45,14 +45,14 @@ def check_argument_presence_and_value(argument,
         raise TypeError
 
 
-def process_args(size_params):
+def check_and_process_args(size_params):
     args = add_arguments()
     if not os.path.isfile(args.filepath):
         raise IOError
-    check_argument_presence_and_value(args.width, 'width', size_params)
-    check_argument_presence_and_value(args.height, 'height', size_params)
-    check_argument_presence_and_value(args.scale,
-                                      'scale', size_params, type='float')
+    check_size_argument_presence_and_value(args.width, 'width', size_params)
+    check_size_argument_presence_and_value(args.height, 'height', size_params)
+    check_size_argument_presence_and_value(args.scale,
+                                           'scale', size_params, type='float')
     if any(key in ['width', 'height'] for key in size_params.keys()):
         if 'scale' in size_params.keys():
             raise AttributeError
@@ -66,74 +66,100 @@ def open_image(filepath):
     return source_image
 
 
+def print_alert(message):
+    print(message)
+
+
 def compute_result_size(source_size, size_params):
     if 'scale' in size_params:
         return (int(source_size['width'] * size_params['scale']),
                 int(source_size['height'] * size_params['scale']),)
     if 'width' in size_params and 'height' in size_params:
-        print('Scale of source image will not be saved')
-        return (size_params['width'], size_params['height'],)
+        print_alert('Scale of source image will not be saved')
+        return (size_params['width'], size_params['height'], )
     if 'width' in size_params:
-        height = int((size_params['width'] / source_size['width']) * source_size['height'])
+        height = int((
+                        size_params['width'] / source_size['width']
+                     ) * source_size['height'])
         return (size_params['width'], height,)
     if 'height' in size_params:
-        width = int((size_params['height'] / source_size['height']) * source_size['width'])
+        width = int((
+                        size_params['height'] / source_size['height']
+                    ) * source_size['width'])
         return (width, size_params['height'],)
 
 
-def resize_image(source_image, source_size, size_params, output_size):
+def generate_output_size_dict(output_size_tuple, output_size_dict):
+    output_size_dict['width'] = output_size_tuple[WIDTH_INDEX]
+    output_size_dict['height'] = output_size_tuple[HEIGHT_INDEX]
+
+
+def resize_image(source_image, source_size, size_params, output_size_dict):
     output_size_tuple = compute_result_size(source_size, size_params)
-    output_size['width'] = output_size_tuple[WIDTH_INDEX]
-    output_size['height'] = output_size_tuple[HEIGHT_INDEX]
+    generate_output_size_dict(output_size_tuple, output_size_dict)
     resized_image = source_image.resize(output_size_tuple)
     return resized_image
 
 
-def get_size_from_thumbnails(source_image):
+def get_size_from_source_image(source_image):
     size = {}
     size['width'] = source_image.size[WIDTH_INDEX]
     size['height'] = source_image.size[HEIGHT_INDEX]
     return size
 
 
-def save_image(args, resized_image, output_size):
+def create_output_params_dict(args, output_size_dict):
+    output_params_dict = {}
     source_image_dirs, source_image_name = os.path.split(args.filepath)
-    source_image_name_parts = source_image_name.split('.')
-    output_image_name = '{}__{}x{}.{}'.format(source_image_name_parts[0],
-                                              output_size['width'],
-                                              output_size['height'],
-                                              source_image_name_parts[1])
+    source_image_name_part, source_image_ext_part = source_image_name.split('.')
+    output_image_name = '{}__{}x{}.{}'.format(
+        source_image_name_part,
+        output_size_dict['width'],
+        output_size_dict['height'],
+        source_image_ext_part)
     if not getattr(args, 'output'):
-        output_path = source_image_dirs + '/' + output_image_name
-        message = 'Image saved at {} as {}'.format(source_image_dirs, output_image_name)
+        output_params_dict['output_path'] = source_image_dirs + '/' + output_image_name
+        output_params_dict['message'] = 'Image saved at {} as {}'.format(
+            source_image_dirs, output_image_name)
     else:
-        output_path = args.output + output_image_name
-        message = 'Image saved at {} as {}'.format(args.output, output_image_name)
-    resized_image.save(output_path)
-    return message
+        output_params_dict['output_path'] = args.output + output_image_name
+        output_params_dict['message'] = 'Image saved at {} as {}'.format(
+            args.output, output_image_name)
+    return output_params_dict
+
+
+def save_image(args, resized_image, output_size_dict):
+    output_params_dict = create_output_params_dict(args, output_size_dict)
+    resized_image.save(output_params_dict['output_path'])
+    return output_params_dict['message']
 
 
 if __name__ == '__main__':
     size_params = {}
-    output_size = {}
+    output_size_dict = {}
     try:
-        args = process_args(size_params)
+        args = check_and_process_args(size_params)
     except IOError:
         sys.exit('Presented file is not exists')
     except AttributeError:
-        sys.exit('Incompatible size arguments introduced. Need arguments: width or height (or both), or scale only!')
+        sys.exit('Incompatible size arguments introduced. '
+                 'Need arguments: width or height (or both), or scale only!')
     except TypeError:
         sys.exit('Size arguments must be only numeric!')
     except RuntimeError:
         sys.exit('Path to store output file is not correct!')
     try:
         source_image = open_image(args.filepath)
-        source_size = get_size_from_thumbnails(source_image)
-        resized_image = resize_image(source_image, source_size, size_params, output_size)
+        source_size = get_size_from_source_image(source_image)
+        resized_image = resize_image(source_image,
+                                     source_size,
+                                     size_params,
+                                     output_size_dict)
         source_image.close()
-        message = save_image(args, resized_image, output_size)
-        print(message)
+        message = save_image(args, resized_image, output_size_dict)
+        print_alert(message)
     except PermissionError:
-        sys.exit('Something wrong with a file. Check if output path correct!')
+        sys.exit('Something wrong with a file.'
+                 'Check if output path correct!')
     except IOError:
         sys.exit('Unable to open file {}!'.format(args.filepath))
