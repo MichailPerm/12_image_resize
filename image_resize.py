774dd075e26fda2df6b728e03a4673314458a1a2
Michail_Z <apxahrejl@gmail.com>
2018-05-15 00:10:04 +0500

    Style refactoring

--- image_resize.py
+++ image_resize.py
@@ -14,19 +14,19 @@ def add_arguments():
                'cause your computer freeze!')
     parser.add_argument(
         '-f', '--filepath',
-        help='path to original image file')
+        help='path to original img file')
     parser.add_argument(
         '-w', '--width',
-        help='With of result image. Optional. May be the only one.')
+        help='With of result img. Optional. May be the only one.')
     parser.add_argument(
         '-ht', '--height',
-        help='Height of result image. Optional. May be the only one.')
+        help='Height of result img. Optional. May be the only one.')
     parser.add_argument(
         '-s', '--scale',
-        help='Scale of result image. Optional. Must be only one!')
+        help='Scale of result img. Optional. Must be only one!')
     parser.add_argument(
         '-o', '--output',
-        help='Path to store result image. Optional.')
+        help='Path to store result img. Optional.')
     return parser.parse_args()
 
 
@@ -51,17 +51,21 @@ def check_and_process_args(size_params):
         alarm_message = 'Presented file is not exists'
         return alarm_message
     try:
-        check_size_argument_presence_and_value(args.width, 'width', size_params)
-        check_size_argument_presence_and_value(args.height, 'height', size_params)
-        check_size_argument_presence_and_value(args.scale,
-                                               'scale', size_params, arg_type='float')
+        check_size_argument_presence_and_value(
+            args.width, 'width', size_params)
+        check_size_argument_presence_and_value(
+            args.height, 'height', size_params)
+        check_size_argument_presence_and_value(
+            args.scale,
+            'scale', size_params, arg_type='float')
     except RuntimeError:
         alarm_message = 'Size arguments must be only numeric!'
         return alarm_message
     if any(key in ['width', 'height'] for key in size_params.keys()):
         if 'scale' in size_params.keys():
             alarm_message = 'Incompatible size arguments introduced. '
-            alarm_message += 'Need arguments: width or height (or both), or scale only!'
+            alarm_message += 'Need arguments: width or height (or both),'
+            alarm_message += 'scale only!'
             return alarm_message
     if args.output and not os.path.isdir(args.output):
         alarm_message = 'Path to store output file is not correct!'
@@ -69,9 +73,9 @@ def check_and_process_args(size_params):
     return args
 
 
-def open_image(filepath):
-    source_image = Image.open(filepath)
-    return source_image
+def open_img(filepath):
+    source_img = Image.open(filepath)
+    return source_img
 
 
 def print_alert(message):
@@ -83,7 +87,7 @@ def compute_result_size(source_size, size_params):
         return (int(source_size['width'] * size_params['scale']),
                 int(source_size['height'] * size_params['scale']),)
     if 'width' in size_params and 'height' in size_params:
-        print_alert('Scale of source image will not be saved')
+        print_alert('Scale of source img will not be saved')
         output_size_tuple = (size_params['width'], size_params['height'], )
     if 'width' in size_params:
         height = int((
@@ -103,56 +107,56 @@ def generate_output_size_dict(output_size_tuple, output_size_dict):
     output_size_dict['height'] = output_size_tuple[HEIGHT_INDEX]
 
 
-def resize_image(source_image, source_size, size_params, output_size_dict):
+def resize_img(source_img, source_size, size_params, output_size_dict):
     output_size_tuple = compute_result_size(source_size, size_params)
     generate_output_size_dict(output_size_tuple, output_size_dict)
-    resized_image = source_image.resize(output_size_tuple)
-    return resized_image
+    resized_img = source_img.resize(output_size_tuple)
+    return resized_img
 
 
-def get_size_from_source_image(source_image):
+def get_size_from_source_img(source_img):
     size = {}
-    size['width'] = source_image.size[WIDTH_INDEX]
-    size['height'] = source_image.size[HEIGHT_INDEX]
+    size['width'] = source_img.size[WIDTH_INDEX]
+    size['height'] = source_img.size[HEIGHT_INDEX]
     return size
 
 
 def create_output_params_dict(args, output_size_dict):
     output_params_dict = {}
-    source_image_dirs, source_image_name = os.path.split(args.filepath)
-    source_image_name_part, source_image_ext_part = source_image_name.split('.')
-    output_image_name = '{}__{}x{}.{}'.format(
-        source_image_name_part,
+    source_img_dirs, source_img_name = os.path.split(args.filepath)
+    source_img_name_part, source_img_ext_part = source_img_name.split('.')
+    output_img_name = '{}__{}x{}.{}'.format(
+        source_img_name_part,
         output_size_dict['width'],
         output_size_dict['height'],
-        source_image_ext_part)
+        source_img_ext_part)
     if not getattr(args, 'output'):
-        output_params_dict['output_path'] = source_image_dirs + '/' + output_image_name
-        output_params_dict['message'] = 'Image saved at {} as {}'.format(
-            source_image_dirs, output_image_name)
+        output_params_dict['output_path'] = source_img_dirs + '/' + output_img_name
+        output_params_dict['message'] = 'img saved at {} as {}'.format(
+            source_img_dirs, output_img_name)
     else:
-        output_params_dict['output_path'] = args.output + output_image_name
-        output_params_dict['message'] = 'Image saved at {} as {}'.format(
-            args.output, output_image_name)
+        output_params_dict['output_path'] = args.output + output_img_name
+        output_params_dict['message'] = 'img saved at {} as {}'.format(
+            args.output, output_img_name)
     return output_params_dict
 
 
-def save_image(args, resized_image, output_size_dict):
+def save_img(args, resized_img, output_size_dict):
     output_params_dict = create_output_params_dict(args, output_size_dict)
-    resized_image.save(output_params_dict['output_path'])
+    resized_img.save(output_params_dict['output_path'])
     return output_params_dict['message']
 
 
-def process_image(args, size_params):
+def process_img(args, size_params):
     output_size_dict = {}
-    source_image = open_image(args.filepath)
-    source_size = get_size_from_source_image(source_image)
-    resized_image = resize_image(source_image,
-                                 source_size,
-                                 size_params,
-                                 output_size_dict)
-    source_image.close()
-    message = save_image(args, resized_image, output_size_dict)
+    source_img = open_img(args.filepath)
+    source_size = get_size_from_source_img(source_img)
+    resized_img = resize_img(source_img,
+                             source_size,
+                             size_params,
+                             output_size_dict)
+    source_img.close()
+    message = save_img(args, resized_img, output_size_dict)
     return message
 
 
@@ -161,8 +165,5 @@ if __name__ == '__main__':
     args = check_and_process_args(size_params)
     if type(args) == str:
         sys.exit(args)
-
-    message = process_image(args, size_params)
+    message = process_img(args, size_params)
     print_alert(message)
-
-
